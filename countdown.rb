6e5ccc6045ed31ff9970eed642f1d501c25b31af
DANIELLE ALBA <danib.sgh@gmail.com>
2019-02-21 21:54:07 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,26 @@
-#write your code here
+def countdown(num)
+# num
 
-def countdown
+while num > 0 do
+
+puts "#{num} SECOND(S)!"
+
+num -= 1
+
 end
+"HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(num)
+# num
+
+while num > 0 do
+
+puts "#{num} SECOND(S)!"
+sleep(1)
+num -= 1
+
+end
+"HAPPY NEW YEAR!"
+end
+countdown(15
